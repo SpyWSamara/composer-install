639ab523cf1d9fd087179c119823cdc7edd935b1
SpyWSamara <SpyWSamara@gmail.com>
2021-04-01 15:06:25 +0400

    Bitrix setup script

---
+++ bitrix.php
@@ -0,0 +1,71 @@
+<?php
+
+use Bitrix\Main\Application;
+use Bitrix\Main\IO\Directory;
+use Bitrix\Main\IO\File;
+
+$php = '[path-to-php-cli]';
+$composerPath = '[path-to-composer.phar]';
+if (empty($php)) {
+    throw new \Exception('Path to php cli cant be empty!');
+}
+if (!\file_exists($php)) {
+    throw new \Exception('Path to php cli not exists!');
+}
+if (empty($composerPath)) {
+    throw new \Exception('Path to composer cant be empty!');
+}
+if (!\file_exists($composerPath)) {
+    throw new \Exception('Path to composer not exists!');
+}
+
+$path = new Directory(
+    Application::getDocumentRoot().'/local'
+);
+
+if (!$path->isExists()) {
+    $path->create();
+}
+
+$composer = new File(
+    $path->getPath().'/composer.json'
+);
+$composerConfig = [
+    'config' => [
+        'sort-packages' => true,
+        'optimize-autoloader' => true,
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Local\\' => 'src/',
+        ],
+    ],
+    'require' => [
+        'wikimedia/composer-merge-plugin' => 'dev-master',
+    ],
+    'extra' => [
+        'merge-plugin' => [
+            'require' => [
+                '../bitrix/composer-bx.json',
+            ],
+        ],
+    ],
+];
+$composerJson = \json_encode(
+    $composerConfig,
+    JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT
+);
+$composer->putContents($composerJson);
+
+$composerBin = new File(
+    $path->getPath().'/composer'
+);
+$composerBinAlias = \sprintf(
+    '%s -c %s -f %s $*',
+    $php,
+    \php_ini_loaded_file(),
+    $composerPath
+);
+$composerBin->putContents($composerBinAlias);
+// 0111 - executable for all
+\chmod($composerBin->getPath(), $composerBin->getPermissions() | 0111);
